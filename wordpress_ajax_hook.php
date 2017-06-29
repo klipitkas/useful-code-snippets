221e11fa3232ae40f7c82620d4bac87aa165323d
Konstantinos Lypitkas <klipitkas@gmail.com>
2017-06-29 19:00:29 +0300

    Add wordpress client side js example code

--- wordpress_ajax_hook.php
+++ wordpress_ajax_hook.php
@@ -14,3 +14,23 @@ add_action( 'wp_ajax_ajax_func_name', 'ajax_func_name');
 
 /* Public access */
 add_action( 'wp_ajax_nopriv_ajax_func_name', 'ajax_func_name');
+
+/*
+$.ajax({
+    url: ajax_news_data.ajaxurl,
+    method: 'POST',
+    dataType: 'json',
+    data: {
+        action: ajax_news_data.action,
+        limit: load_limit,
+        offset: starting_offset,
+        lang: $('input[name=language]').val(),
+    },
+    success: function(data, textStatus, jqxhr) {
+        if ( data.length == 0 ) {
+            no_more_posts.show();
+            self_btn.hide();
+        }
+    }
+});
+*/
